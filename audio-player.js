14bd58aac1d71e7f4d02dd23e50ede6fde6337ed
Daniel Jettka <research@jettka.de>
2024-03-28 15:22:00 +0100

    first core for web component

---
+++ audio-player.js
@@ -0,0 +1,50 @@
+class AudioPlayer extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+  }
+
+  connectedCallback() {
+    this.render();
+    this.addEventListeners();
+  }
+
+  render() {
+    this.shadowRoot.innerHTML = `
+      <style>
+        audio {
+          width: 100%;
+        }
+        button {
+          display: block;
+          margin: 10px 0;
+        }
+      </style>
+      <audio controls>
+        <source src="" type="">
+        Your browser does not support the audio element.
+      </audio>
+      ${this.getTrackButtons()}
+    `;
+  }
+
+  getTrackButtons() {
+    const tracks = JSON.parse(this.getAttribute('tracks'));
+    return tracks.map(track => `<button data-src="${track.src}" data-type="${track.type}">${track.name}</button>`).join('');
+  }
+
+  addEventListeners() {
+    this.shadowRoot.querySelectorAll('button').forEach(button => {
+      button.addEventListener('click', (event) => {
+        const audio = this.shadowRoot.querySelector('audio');
+        const source = this.shadowRoot.querySelector('source');
+        source.src = event.target.dataset.src;
+        source.type = event.target.dataset.type;
+        audio.load();
+        audio.play();
+      });
+    });
+  }
+}
+
+customElements.define('audio-player', AudioPlayer);
